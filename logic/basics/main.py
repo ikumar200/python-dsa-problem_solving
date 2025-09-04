abb1457f27ed47a51257067c7469253bc7f2d8ed
indrakumar <indrakumarhn01@gmail.com>
2025-09-04 17:42:01 +0000

    completed some logical basics questions

---
+++ logic/basics/main.py
@@ -0,0 +1,47 @@
+# def isEvenOdd(n):
+#     if n%2==0:
+#         print("EVEN")
+#     else:
+#         print("ODD")
+
+# if __name__=="__main__":
+#     isEvenOdd(15)
+
+# -----------------------------------------------------
+
+# multiplication table
+# if __name__=="__main__":
+#     n=5
+#     for i in range(1,11):
+#         print(n*i)
+
+# -----------------------------------------------------
+# sum of n natural numbers
+# def naturalSum(n):
+#     if n==1:
+#         return 1
+#     return n+ naturalSum(n-1)
+
+# if __name__=="__main__":
+#     n=10
+#     print(naturalSum(n))
+
+# -----------------------------------------------------
+#sum of squares
+# n=4
+# res=0
+# for i in range(1,n+1):
+#     res=res+i*i
+# print(res)
+
+# ------------------------------------------------------
+# two num swap
+# a=5
+# b=7
+# print(f'before a={a} and b={b}')
+# a=a+b
+# b=a-b
+# a=a-b
+
+# print(f'before a={a} and b={b}')
+
---
+++ logic/main.py
@@ -0,0 +1,11 @@
+# def isEven(n):
+#     return n%2==0
+
+# if __name__=="__main__":
+#     n=15
+#     if isEven(n):
+#         print("EVEN")
+#     else:
+#         print("ODD")
+
+print('even')
